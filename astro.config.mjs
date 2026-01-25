b6a1644ba3d728c2252eb759332784973423bd12
Javi Rodríguez Viñas <javi.vinas.2000@gmail.com>
2026-01-25 20:07:34 +0100

    feat: Implement SEO improvements with sitemap, robots.txt, and enhanced meta tags, and add GitHub Actions for automated deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://JaviVinnas.github.io',
+  base: '/personal-site',
+  build: {
+    sourcemap: true,
+  },
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
